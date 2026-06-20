f969ae9e3cc58e54fdb8a91163926eb5412646bf
Jatin Patel <me@jatinpatel.ca>
2026-06-20 14:51:56 -0400

    1590. Make Sum Divisible by P

---
+++ problems/make_sum_divisible_by_p.py
@@ -0,0 +1,53 @@
+"""
+https://leetcode.com/problems/make-sum-divisible-by-p/
+
+Given an array of positive integers nums, remove the smallest subarray (possibly empty) such that the sum of the remaining elements is divisible by p. It is not allowed to remove the whole array.
+
+Return the length of the smallest subarray that you need to remove, or -1 if it's impossible.
+
+A subarray is defined as a contiguous block of elements in the array.
+
+
+
+Example 1:
+
+Input: nums = [3,1,4,2], p = 6
+Output: 1
+Explanation: The sum of the elements in nums is 10, which is not divisible by 6. We can remove the subarray [4], and the sum of the remaining elements is 6, which is divisible by 6.
+Example 2:
+
+Input: nums = [6,3,5,2], p = 9
+Output: 2
+Explanation: We cannot remove a single element to get a sum divisible by 9. The best way is to remove the subarray [5,2], leaving us with [6,3] with sum 9.
+Example 3:
+
+Input: nums = [1,2,3], p = 3
+Output: 0
+Explanation: Here the sum is 6. which is already divisible by 3. Thus we do not need to remove anything.
+
+
+Constraints:
+
+1 <= nums.length <= 10^5
+1 <= nums[i] <= 10^9
+1 <= p <= 10^9
+"""
+
+
+class Solution:
+    def minSubarray(self, nums: list[int], p: int) -> int:
+        r = sum(nums) % p
+        if r == 0:
+            return 0
+
+        map: dict[int, int] = {0: -1}
+        ans = len(nums)
+        prefix_j = 0
+        for i, num in enumerate(nums):
+            prefix_j = (prefix_j + num) % p
+            prefix_i = (prefix_j - r) % p
+            if prefix_i in map:
+                ans = min(ans, i - map[prefix_i])
+            map[prefix_j] = i
+
+        return ans if ans < len(nums) else -1
