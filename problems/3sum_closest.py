fe1a801524b432ffa21f11701299080fcdf3763a
Jatin Patel <me@jatinpatel.ca>
2026-06-23 06:04:23 -0400

    16. 3Sum Closest

---
+++ problems/3sum_closest.py
@@ -0,0 +1,52 @@
+"""
+https://leetcode.com/problems/3sum-closest/
+
+Given an integer array nums of length n and an integer target, find three integers at distinct indices in nums such that the sum is closest to target.
+
+Return the sum of the three integers.
+
+You may assume that each input would have exactly one solution.
+
+
+
+Example 1:
+
+Input: nums = [-1,2,1,-4], target = 1
+Output: 2
+Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+Example 2:
+
+Input: nums = [0,0,0], target = 1
+Output: 0
+Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+
+
+Constraints:
+
+3 <= nums.length <= 500
+-1000 <= nums[i] <= 1000
+-10^4 <= target <= 10^4
+"""
+
+
+class Solution:
+    def threeSumClosest(self, nums: list[int], target: int) -> int:
+        nums.sort()
+        closest_sum = sum(nums[:3])
+
+        for i in range(len(nums) - 2):
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                curr_sum = nums[i] + nums[left] + nums[right]
+
+                if abs(target - curr_sum) < abs(target - closest_sum):
+                    closest_sum = curr_sum
+
+                if curr_sum < target:
+                    left += 1
+                elif curr_sum > target:
+                    right -= 1
+                else:
+                    return curr_sum
+
+        return closest_sum
