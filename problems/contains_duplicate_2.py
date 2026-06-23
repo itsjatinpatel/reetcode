f0d74e2d9491096896e5e1c4e4d3a003dcf40ab4
Jatin Patel <me@jatinpatel.ca>
2026-06-23 07:05:38 -0400

    219. Contains Duplicate 2

---
+++ problems/contains_duplicate_2.py
@@ -0,0 +1,39 @@
+"""
+https://leetcode.com/problems/contains-duplicate-ii/
+
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3
+Output: true
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1
+Output: true
+Example 3:
+
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+
+
+Constraints:
+
+1 <= nums.length <= 10^5
+-10^9 <= nums[i] <= 10^9
+0 <= k <= 10^5
+"""
+
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
+        window = set()
+        for idx, num in enumerate(nums):
+            if num in window:
+                return True
+            window.add(num)
+            if len(window) > k:
+                window.remove(nums[idx - k])
+        return False
